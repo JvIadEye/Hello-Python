59744ddd645ef940d92bc135e58b236cb5d6fe17
HaiAuHapHoi <135623930+JvIadEye@users.noreply.github.com>
2025-10-20 16:23:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
+# app.py
 from flask import Flask
+import os
+
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def hello():
-    return "Hello OCP version 1.2"
+    # Dòng text mong muốn
+    return 'Xin chào! Ứng dụng này đã được triển khai bằng OpenShift GitOps.'
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+if __name__ == '__main__':
+    # Sử dụng cổng mặc định của OpenShift
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
+
